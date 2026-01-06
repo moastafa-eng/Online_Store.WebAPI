785cdff95eb989ff7fcbd2f009bf15fbcc8f8dbb
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmail.com>
2026-01-06 12:29:11 +0200

    Prepare Class Specifications

---
+++ Core/Domain/Contracts/ISpecifications.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Domain.Contracts
+{
+    public interface ISpecifications<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        // **Query Sections**
+
+        // For Include Expression section
+        List<Expression<Func<TEntity, object>>> Includes { get; set; }
+
+        // For Filtration Expression section
+        // Null able property
+        Expression<Func<TEntity, bool>>? Criteria { get; set; }
+    }
+}
---
+++ Core/Services/Specifications/BaseSpecifications.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Contracts;
+using Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Services.Specifications
+{
+    public class BaseSpecifications<TEntity, TKey> : ISpecifications<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        // **Query Sections Implementation**
+        public List<Expression<Func<TEntity, object>>> Includes { get; set; } = new List<Expression<Func<TEntity, object>>>();
+        public Expression<Func<TEntity, bool>>? Criteria { get; set; }
+
+        public BaseSpecifications(Expression<Func<TEntity, bool>>? expression) // Null able expression
+        {
+            Criteria = expression;
+        }
+    }
+}
