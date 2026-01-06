a3fc4f72461661dc47d264c62a0af4b0766fd661
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmail.com>
2026-01-06 14:14:29 +0200

    Built Specification Evaluator

---
+++ infrastructure/Persistence/SpecificationsEvaluator.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Contracts;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public static class SpecificationsEvaluator
+    {
+        // **Create Query**
+
+        public static IQueryable<TEntity> GetQuery<TEntity, TKey>(IQueryable<TEntity> inputQuery,
+            ISpecifications<TEntity, TKey> spec) where TEntity : BaseEntity<TKey>
+        {
+            var query = inputQuery; // _context.Products
+
+            // check if Criteria is not null
+            if(spec.Criteria is not null)
+            {
+                query = query.Where(spec.Criteria); // _context.Products.Where('Filter Expression')
+            }
+
+            // aggregate => concatenation
+            // _context.Products.Where('expression').Include('includeExpression').Inlude('includeExpression')+...
+            spec.Includes.Aggregate(query, (query, includeExpression) => query.Include(includeExpression));
+
+
+            return query;
+        }
+    }
+}
