7a9b59f66883b6acdae07e0013fd83ec6825531c
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmail.com>
2026-01-28 17:54:16 +0200

    Not Found End Point Error Handling

--- Core/Domain/Exceptions/NotFoundEx/NotFoundEx.cs
+++ Core/Domain/Exceptions/NotFoundEx/NotFoundEx.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Exceptions.NotFoundEx
+﻿namespace Domain.Exceptions.NotFoundEx
 {
     public abstract class NotFoundEx(string message) : Exception(message)
     {
--- Online_Store.Web/MiddleWares/GlobalErrorHandlingMiddleWare.cs
+++ Online_Store.Web/MiddleWares/GlobalErrorHandlingMiddleWare.cs
@@ -6,18 +6,37 @@ namespace Online_Store.Web.MiddleWares
 {
     public class GlobalErrorHandlingMiddleWare
     {
-        private readonly RequestDelegate _next;
+        private readonly RequestDelegate _next; // _next : contains the address of the next middle ware
 
         public GlobalErrorHandlingMiddleWare(RequestDelegate next)
         {
             _next = next;
         }
 
-        public async Task InvokeAsync(HttpContext context)
+        // HttpContext : contains the information about Http request and Http response
+        public async Task InvokeAsync(HttpContext context) 
         {
+            // Try to transfer the Http response to the next middle ware
+            // if an exception occurs while this operation return response with specific specifications.
             try
             {
                 await _next.Invoke(context);
+
+                // Catch the response from Routing Middle Ware and change response body.
+                if(context.Response.StatusCode is StatusCodes.Status404NotFound)
+                {
+                    // Set content type.
+                    context.Response.ContentType = "Application/Json";
+
+                    // set body of the response
+                    var response = new ErrorDetails
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        ErrorMessage = $"The end point with name '{context.Request.Path}' is not found!"
+                    };
+
+                    await context.Response.WriteAsJsonAsync(response);
+                }
             }
             catch(Exception ex)
             {
@@ -39,7 +58,7 @@ namespace Online_Store.Web.MiddleWares
                 };
 
                 // return Response
-                await context.Response.WriteAsJsonAsync(response);
+                await context.Response.WriteAsJsonAsync(response); //  return response with Json format
             }
         }
     }
