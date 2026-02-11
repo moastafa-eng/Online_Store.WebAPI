b8ccf43b2d1c49d9478e5f6c7fa7a71ae94d2644
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmail.com>
2026-02-11 17:48:30 +0200

    Add CacheRepository

---
+++ Core/Domain/Contracts/ICacheRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Contracts
+{
+    public interface ICacheRepository
+    {
+        Task<string?> GetAsync(string key);
+        Task SetAsync(string Key, object value, TimeSpan duration);
+    }
+}
---
+++ infrastructure/Persistence/Repositories/CacheRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Contracts;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace Persistence.Repositories
+{
+    public class CacheRepository(IConnectionMultiplexer _connection) : ICacheRepository
+    {
+        private readonly IDatabase _database = _connection.GetDatabase();  
+        public async Task<string?> GetAsync(string key)
+        {
+            return await _database.StringGetAsync(key);
+        }
+
+        public async Task SetAsync(string key, object value, TimeSpan duration)
+        {
+            await _database.StringSetAsync(key, JsonSerializer.Serialize(value), duration);
+        }
+    }
+}
