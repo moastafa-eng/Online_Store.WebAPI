08a5620df8bde258d14c383bb585ff14f05667f3
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmail.com>
2026-02-18 09:33:30 +0200

    Security Module - Auth Controller[Login - Register]

---
+++ infrastructure/Presentation/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+using Shard.DTOs.Auth;
+
+namespace Presentation
+{
+    [ApiController]
+    [Route("api/{controller}")]
+    public class AuthController(IServiceManager _serviceManager) : ControllerBase
+    {
+        [HttpPost("Login")] // Url : BaseUrl/api/Auth/Login
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            var result = await _serviceManager.AuthService.LoginAsync(request);
+            return Ok(result);
+        }
+
+        [HttpPost("Register")] // Url : BaseUrl/api/Auth/Register
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            var result = await _serviceManager.AuthService.RegisterASync(request);
+            return Ok(result);
+        }
+    }
+}
