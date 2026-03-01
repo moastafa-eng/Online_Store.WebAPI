5cb134eb3c1d27d66d35705208bc9fa0a9a26a09
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmail.com>
2026-03-01 15:38:51 +0200

    Payment Module-DAL

--- Core/Domain/Entities/Baskets/CustomerBasket.cs
+++ Core/Domain/Entities/Baskets/CustomerBasket.cs
@@ -8,7 +8,6 @@
         // every item contains a product details like price, name and quantity
         public IEnumerable<BasketItem> Items { get; set; }
 
-
         
         public int? DeliveryMethodId { get; set; }
         public string? PaymentIntentId { get; set; }
